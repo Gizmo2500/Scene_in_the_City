245c7b8e2ca0e471feecba34a7e4d28951c30c99
Alex Trzeciak <Trebek@Alexs-MacBook-Air.local>
2015-05-08 00:45:26 -0700

    Changed Database from Sqlite3 to postgreSQL

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150507014751) do
+ActiveRecord::Schema.define(version: 20150508050143) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "locations", force: :cascade do |t|
     t.string   "movie"
@@ -19,6 +22,16 @@ ActiveRecord::Schema.define(version: 20150507014751) do
     t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "profiles", force: :cascade do |t|
+    t.string   "name"
+    t.string   "origin"
+    t.string   "location_pic"
+    t.text     "comment"
+    t.integer  "user_id"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
   end
 
   create_table "users", force: :cascade do |t|
@@ -36,7 +49,7 @@ ActiveRecord::Schema.define(version: 20150507014751) do
     t.datetime "updated_at"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
