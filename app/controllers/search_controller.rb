cc86bf67a5ecfab689cea4aae5962a478319d204
Alex Trzeciak <Trebek@Alexs-MacBook-Air.local>
2015-05-12 17:08:03 -0700

    Added location information to popup box

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,6 +3,7 @@ class SearchController < ApplicationController
   	if params[:q]
   	resp = Typhoeus.get("https://data.sfgov.org/resource/yitu-d5am.json", :params => {:title => params[:q]})
   	json_resp = JSON.parse(resp.body)
+    
   	@search = json_resp
     @movie = []
     @location = []
