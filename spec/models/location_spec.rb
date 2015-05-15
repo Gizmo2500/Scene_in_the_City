8cf18af03c43c6cb4c4d94e33cd4bcc206dff87a
Alex Trzeciak <Trebek@Alexs-MacBook-Air.local>
2015-05-14 17:49:37 -0700

    create location spec

--- spec/models/location_spec.rb
+++ spec/models/location_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Location, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+    
+
+  it "creates a location" do #Verify that a new location can be created 
+    @location = Location.new
+    expect(@location).to be_instance_of Location
+  end
+
+  it "creates a location" do  #Verify that authentication works 
+    @location = Location.create(  user_id: "1",
+      movie: "Godzilla", location: "Golden Gate Bridge")
+    expect(@location).to_not be_valid 
+  end
+
 end
+
